6aceee4abafd5577d6c92c71ad7a174b38c62af1
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-20 15:18:54 -0300

    Start dependences from chatbot

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('specific', views.specific, name='specific')
+    path('specific', views.specific, name='specific'),
+    path('getResponse', views.getresponse, name='getResponse'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot = ChatBot('chatbot', read_only=False, logic_adapters=['chatterbot.logic.BestMatch'])
+
+list_to_train = [
+    "hi",
+    "hi there",
+    "What's your name?",
+    "I am just a chatbot",
+    "What is your favorite food?",
+    "I like Pizza"
+]
+
+list_trainer = ListTrainer(bot)
+list_trainer.train(list_to_train)
 
 def index(request):
     return render(request, 'blog/index.html')
 
 def specific(request):
     return render(request)
+
+
+def getresponse(request):
+    userMessage = request.GET.get('userMessage')
+    return HttpResponse(userMessage)
